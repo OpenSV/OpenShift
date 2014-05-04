e7b61fa526e18515ac90d811d33bb77628218c05
David Thomson <dt@appjacket.com>
2014-05-04 18:13:21 -0400

    added a url handler to app.rb for popcornjs.html updated index.html

--- app.rb
+++ app.rb
@@ -5,11 +5,14 @@ require 'sinatra'
 get '/' do
 	send_file 'app/index.html'
 end
-get '/videojs' do
-	send_file 'app/partials/videojs.html'
-end
 get '/jPlayer' do
 	send_file 'app/partials/jPlayer.html'
+end
+get '/popcornjs' do
+	send_file 'app/partials/popcornjs.html'
+end
+get '/videojs' do
+	send_file 'app/partials/videojs.html'
 end
 get '/*' do
         send_file "#{params[:splat][0]}"
